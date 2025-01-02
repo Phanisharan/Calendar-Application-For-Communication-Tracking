403fc67229daa487aa9d4aa9ba0d67748d76e9fa
PhaniSharan Boini <143081814+Phanisharan@users.noreply.github.com>
2025-01-02 12:05:45 +0530

    new

--- communication_tracker/api/index.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Set the settings module for the Django application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'communication_tracker.settings')
-
-# Get the Django WSGI application
-app = get_wsgi_application()
